36c2f8ea97e2101b5f259a563d8de7508dd0c331
regularcoder123 <atharva.78722@gmail.com>
2025-09-25 17:15:56 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+function multiply(a,b){
+    if (a == undefined || b == undefined){
+        return "Both numbers must be defined"
+    }
+    else{
+        return a * b;
+    }
+}
+
+console.log(multiply(2,4))
